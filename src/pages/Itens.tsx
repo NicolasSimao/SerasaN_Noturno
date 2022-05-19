824f3573def9121b522fea5ee30cbc8e71e7ec0b
NicolasSimao <105399462+NicolasSimao@users.noreply.github.com>
2022-05-19 20:19:54 -0300

    adicionar formulario de criação do botão criar

--- src/pages/Itens.tsx
+++ src/pages/Itens.tsx
@@ -4,16 +4,38 @@ import Botao from '../components/Botao';
 import './Itens.css';
 import imagem from './humaaans-space.png';
 const ItensPage = function () {
+   
+   const botaoCriarClicado = function (){
+       alert ('Seja Bem Vindo');
+   }
+   
+   
+   
+   
     return (
         <>
            <Cabecalho />
-            <main> <h1>Itens</h1>
+            <main> 
+                <button onClick= {botaoCriarClicado}>Clique</button>
+                <h1>Itens</h1>
             <ul>
                 <li>Item 1</li>
                 <li>Item 2</li>
                 <li>Item 3</li>
-            </ul>  
-            <Botao texto = "Entrar"/> <Botao texto= "Voltar"/>  
+            </ul>   
+            {(true) && (
+                <form>
+               <label>
+                   Nome:
+                   <input type= "text" placeholder= "Nome" />
+                </label>
+                <label>
+                    Descrição: 
+                    <textarea placeholder="Descrição"></textarea>
+                </label>
+            </form>
+            )} 
+             <Botao texto = "Entrar"/> <Botao texto= "Voltar"/> 
             </main> 
             <img src={imagem} alt="" />
              <Rodape />
